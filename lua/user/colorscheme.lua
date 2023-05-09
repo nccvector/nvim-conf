6c1d863242653ee51e33599c938d149a2d64eda5
faizan.ali <faizan.ali@mujin.co.jp>
2023-05-09 14:03:24 +0900

    dark hard!

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,6 @@
 local colorscheme = "gruvbox"
 
+pcall(vim.cmd, "let " .. "g:gruvbox_contrast_dark='hard'")
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
